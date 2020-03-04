ddfeea93ab30d673a198a51400cabc4ebbf4c596
kyoshicava <55817214+kyoshicava@users.noreply.github.com>
2020-03-04 19:48:52 -0300

    insert de restaurantes e enderecos

--- foodback/src/main/resources/import.sql
+++ foodback/src/main/resources/import.sql
@@ -1,8 +1,44 @@
-insert into Restaurant (id, name, address) values (1, 'Restaurante 1', 'Endereco 1')
-insert into Restaurant (id, name, address) values (2, 'Restaurante 2', 'Endereco 2')
-insert into Restaurant (id, name, address) values (3, 'Restaurante 3', 'Endereco 3')
+insert into Restaurant (id, name, address) values (1, 'Restaurant Trebbiano', 'Alameda Campinas, 266 - Cerqueira César, São Paulo - SP')
+insert into Restaurant (id, name, address) values (2, 'ARABEK', 'R. Pamplona, 684 - Jardim Paulista, São Paulo - SP')
+insert into Restaurant (id, name, address) values (3, 'Temakeria Makis Place', 'Alameda Santos, 1202 - Jardim Paulista, São Paulo - SP')
+insert into Restaurant (id, name, address) values (4, 'Spot', 'Alameda Min. Rocha Azevedo, 72 - Bela Vista, São Paulo - SP')
+insert into Restaurant (id, name, address) values (5, 'Noah Gastronomia', 'Rua Peixoto Gomide, 707 - Cerqueira César, São Paulo - SP')
+insert into Restaurant (id, name, address) values (6, 'Seen - Restaurant & Bar', 'Alameda Santos, 1437 - Jardim Paulista, São Paulo - SP')
+insert into Restaurant (id, name, address) values (7, 'Restaurante Pamplona', 'R. Pamplona, 900 - Jardim Paulista, São Paulo - SP')
+insert into Restaurant (id, name, address) values (8, 'Capim Santo São Paulo', 'Alameda Min. Rocha Azevedo, 471 - Jardins, São Paulo - SP')
+insert into Restaurant (id, name, address) values (9, 'Manai Gastronomia', 'Alameda Santos, 1219 - Cerqueira César, São Paulo - SP')
+insert into Restaurant (id, name, address) values (10, 'Pimenta Rosa', 'Alameda Santos, 831 - Jardim Paulista, São Paulo - SP')
+insert into Restaurant (id, name, address) values (11, 'Abbraccio', 'Avenida Paulista, 2064 Shopping Center 3 - Bela Vista, São Paulo - SP')
+insert into Restaurant (id, name, address) values (12, 'Club Life To Go Paulista', 'Alameda Santos, 1518 - Lojas 71/72 - Jardim Paulista, São Paulo - SP')
 
 insert into Feedback (id, preference, satisfied, comment, restaurantId) values (1, 'Preferencia 1', true, 'Comment 1', 2)
 insert into Feedback (id, preference, satisfied, comment, restaurantId) values (2, 'Preferencia 2', false, 'Comment 2', 2)
 insert into Feedback (id, preference, satisfied, comment, restaurantId) values (3, 'Preferencia 3', true, 'Comment 3', 1)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (4, 'Preferencia 1', true, 'Comment 1', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (5, 'Preferencia 2', false, 'Comment 2', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (6, 'Preferencia 3', true, 'Comment 3', 1)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (7, 'Preferencia 1', true, 'Comment 1', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (8, 'Preferencia 2', false, 'Comment 2', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (9, 'Preferencia 3', true, 'Comment 3', 1)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (10, 'Preferencia 1', true, 'Comment 1', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (11, 'Preferencia 2', false, 'Comment 2', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (12, 'Preferencia 3', true, 'Comment 3', 1)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (13, 'Preferencia 1', true, 'Comment 1', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (14, 'Preferencia 2', false, 'Comment 2', 2)
+insert into Feedback (id, preference, satisfied, comment, restaurantId) values (15, 'Preferencia 3', true, 'Comment 3', 1)
+
+
+--         [1, 'Restaurant Trebbiano',  -23.564188, -46.651951, 0, 'feedback.html?rest=trebbiano', '<br><a>#vegetariano</a><br><a>#petfriendly</a>'],
+--         [2, 'ARABEK', -23.563952, -46.654304,0, 'feedback.html?rest=arabek', '<br><a>#petfriendly</a>'],
+--         [3, 'Temakeria Makis Place', -23.564973, -46.654079,0, 'feedback.html?rest=makisplace', '<br><a>#vegano</a><br><a>#vegetariano</a>'],
+--         [4, 'Spot',  -23.558704, -46.657200, 0, 'feedback.html?rest=spot', '<br><a>#vegetariano</a><br><a>#kidfriendly</a>'],
+--         [5, 'Noah Gastronomia', -23.560000, -46.655849,0, 'feedback.html?rest=noah', '<br><a>#vegano</a>'],
+--         [6, 'Seen - Restaurant & Bar', -23.563866, -46.656070,0, 'feedback.html?rest=seen', '<br><a>#fit</a><br><a>#vegetariano</a>'],
+--	 	 [7, 'Restaurante Pamplona',  -23.565422, -46.655814, 0, 'feedback.html?rest=pamplona', '<br><a>#petfriendly</a>'],
+--         [8, 'Capim Santo São Paulo', -23.562007, -46.659974,0, 'feedback.html?rest=capim', '<br><a>#vegetariano</a><br><a>#petfriendly</a>'],
+--         [9, 'Manai Gastronomia', -23.564743, -46.654513,0, 'feedback.html?rest=manai', '<br><a>#fit</a><br><a>#vegetariano</a>'],
+--	 	 [10, 'Pimenta Rosa',  -23.566375, -46.651664, 0, 'feedback.html?rest=pimenta', '<br><a>#vegetariano</a><br><a>#kidfriendly</a>'],
+--         [11, 'Abbraccio', -23.556909, -46.659031,0, 'feedback.html?rest=abbraccio', '<br><a>#vegetariano</a>'],
+--         [12, 'Club Life To Go Paulista', -23.562835, -46.656557,0, 'feedback.html?rest=club', '<br><a>#fit</a><br><a>#vegano</a><br><a>#restricaonozes</a><br><a>#gluenfree</a><br><a>#vegetariano</a>']
+
 
